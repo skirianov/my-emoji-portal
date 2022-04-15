dae0b6eeb1c07cda1c4c37043c90299ce5c5b75f
Sergii <s.kirianov@gmail.com>
2022-04-15 21:48:04 +0300

    add random person to submit an emoji and console.log total numbers

--- contracts/EmojiPortal.sol
+++ contracts/EmojiPortal.sol
@@ -1,13 +1,24 @@
-// SPDX-License-Identifier: <SPDX-License>
+// SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 
 contract EmojiPortal {
+  uint256 totalEmojis;
+
   constructor() {
     console.log("Contract");
   }
 
+  function submitEmoji() public {
+    totalEmojis += 1;
+    console.log('%s has emojied you', msg.sender);
+  }
+
+  function getTotalEmojis() public view returns (uint256) {
+    console.log('There are total of %s emojies', totalEmojis);
+    return totalEmojis;
+  }
 
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,22 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const emojiContractFactory = await hre.ethers.getContractFactory("EmojiPortal");
   const emojiContract = await emojiContractFactory.deploy();
   await emojiContract.deployed();
+
   console.log('Contract deployed to: ', emojiContract.address);
+  console.log('Contract deployed by: ', owner.address);
+
+  let emojiCount;
+  emojiCount = await emojiContract.getTotalEmojis();
+
+  let emoji = await emojiContract.submitEmoji();
+  await emoji.wait();
+
+  emoji = await emojiContract.connect(randomPerson).submitEmoji();
+  await emoji.wait();
+
+  emojiCount = await emojiContract.getTotalEmojis();
 };
 
 const runMain = async () => {
